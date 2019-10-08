66d312ec623b939054aa2c1817eef6b6a649425d
wwclearlove <1280488753@qq.com>
2019-10-08 20:11:38 +0800

    first commit

---
+++ resources/static/js/header.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('.the-nav').cbFlyout();
+
+	$(window).scroll(function() {
+		var scrollTop = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop).toFixed(0);
+		if (scrollTop > 80){
+			$("#header").addClass("scroll_header")
+		}else {
+			$("#header").removeClass("scroll_header");
+		}
+	});
+});
